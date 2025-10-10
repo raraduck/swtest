db3c99b7a4418bc2c351a8377cdf59a31c379e38
DAEUN KIM <raraducker@gmail.com>
2025-10-10 18:01:06 +0900

    update

--- vstest/week2/__2G-sortbyfreq/p2910_try01.cpp
+++ vstest/week2/__2G-sortbyfreq/p2910_try01.cpp
@@ -13,6 +13,10 @@ struct meta {
     int freq;
     int priority;
 };
+struct meta2 {
+    int freq;
+    int priority;
+};
 int N;
 string C;
 int main(){
@@ -22,26 +26,28 @@ int main(){
     cin >> N >> C;
 
     vector<string> Ns(N);
-    map<string, int> cntNs;
+    map<string, meta2> cntNs;
     vector<meta> sortItemsByFreq;
 
     for(int i=0; i<N; i++) {
         cin >> Ns[i];
-        if (cntNs[Ns[i]] == 0) {
+        if (cntNs[Ns[i]].freq == 0) {
             sortItemsByFreq.push_back({Ns[i], 0, i});
+            cntNs[Ns[i]].priority=i;
         };
-        cntNs[Ns[i]]+=1;
+        cntNs[Ns[i]].freq+=1;
+        // cntNs[Ns[i]].priority = (i < cntNs[Ns[i]].priority) ? i:cntNs[Ns[i]].priority;
     }
     for (auto& p:sortItemsByFreq){
-        p.freq = cntNs[p.key];
+        p.freq = cntNs[p.key].freq;
     }
     sort(
         sortItemsByFreq.begin(), 
         sortItemsByFreq.end(), 
-        [](meta a, meta b){
-            if (a.freq == b.freq) return a.priority < b.priority;
+        [cntNs](meta a, meta b){
+            // if (a.freq == b.freq) return a.priority < b.priority;
+            if (a.freq == b.freq) return cntNs.at(a.key).priority < cntNs.at(b.key).priority;
             return a.freq > b.freq;
-
         }
     );
 
